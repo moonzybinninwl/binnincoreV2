890918a484abb003bd737fffaa102008ee98ecf7
binninwl <moonzyexploits@gmail.com>
2025-11-04 15:38:42 +1100

    Update EntryPoint.cpp

--- Module/EntryPoint.cpp
+++ Module/EntryPoint.cpp
@@ -15,7 +15,23 @@ Some of the environment is already implemented such as the Closures and Script.
 
 Also uh, set the build to release if you're new to this
 
+===================STRUCTURE===========================
+In workspace you'll find > Engine, Environment, Execution and Scheduler.
+- Execution > Contains the execution logic and yielder
+- Environment > Contains all the functions that scripts use. In the 'nodes' folder - you'll find all the libraries
+- Engine > Contains everything you need to change when Roblox updates. It has your offsets and encryptions - if you don't update these, you'll crash on Inject
+- Scheduler > Contains different things like executing, Getting the datamodel, scriptcontext and more
+
+In Exploit you'll find > Bytecode, Service and Utilities
+- Bytecode > Contains bytecode utilities like Reading and Decompressing bytecode - as well as compiling scripts
+- Service > Contains the Teleport handler, what it does is Reinitialize and load all libraries when a new datamodel is found - aka joining a new experience
+- Utilities > They're basically just variables shared around the code. Instead of redfining everything, why not just access it from one file? Just makes everything easier and cleaner imo
+
+======================================================
+That's all for now - also if you're going to say this is chatgpt, please shut the fuck up. It's a fucking guide
+
 If you have any issues, please contact me on Discord - @binninwl_
+Thanks for reading this.
 */
 
 void MainThread()
